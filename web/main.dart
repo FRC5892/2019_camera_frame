71d02ccfed36a8bc9bddff2b31682c1427e053c3
Kai Page <kaibug@gmail.com>
2019-02-11 12:43:39 -0600

    Added placeholder image for testing

--- web/main.dart
+++ web/main.dart
@@ -8,6 +8,13 @@ StreamController<String> streamController = StreamController();
 void main() {
   Timer.periodic(const Duration(seconds: 1), tryConnect);
   streamController.stream.listen(handleMessage);
+  if (window.origin.contains("localhost")) {
+    (querySelector("#stream") as ImageElement)
+      ..style.transform = "initial"
+      // image from placeholder.com
+      // need it self-served bcuz I probz will be connected to robot wifi while testing
+      ..src = "360x240.png";
+  }
 }
 
 void tryConnect([_]) {
