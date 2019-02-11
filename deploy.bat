a464a33642337725cb0d78dfa23f79581f9293d8
Kai Page <kaibug@gmail.com>
2019-02-11 16:24:01 -0600

    The new deploy system works now

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,9 @@
 @echo off
 cmd /c webdev build
 cd build
-7z a dashboard.zip '@.build.manifest' .build.manifest
-psftp -b deploy.bat.psftp admin@10.58.92.2
+7z a dashboard.zip "@.build.manifest" .build.manifest
+psftp -b ..\deploy.bat.psftp admin@10.58.92.2
 del dashboard.zip
+cd ..
 rem That's so webdev doesn't complain next time
-plink -b deploy.bat.plink admin@10.58.92.2
+plink -m deploy.bat.plink admin@10.58.92.2
