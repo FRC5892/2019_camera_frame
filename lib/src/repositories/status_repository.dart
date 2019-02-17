d76b19b340b3a116e27141b925da517209629dcc
Kai Page <kaibug@gmail.com>
2019-02-17 10:23:41 -0600

    Trying to figure out better when the connection is dropped

--- lib/src/repositories/status_repository.dart
+++ lib/src/repositories/status_repository.dart
@@ -16,34 +16,43 @@ class StatusRepository {
   Stream<StatusMessage> connect(String url) {
     bool retry = true;
     StreamSubscription subscription;
+
+    DateTime failAfter;
+
+    var timer = Timer.periodic(const Duration(seconds: 2), (_) {
+      if (failAfter != null && DateTime.now().isAfter(failAfter)) {
+        subscription?.cancel();
+      }
+    });
+
     var controller = StreamController<StatusMessage>(
         onPause: () => subscription.pause(),
         onResume: () => subscription.resume(),
         onCancel: () {
           retry = false;
           subscription.cancel();
+          timer.cancel();
         });
+    
+    void handleMessage(msg) {
+      if (msg is String) {
+        //print(msg);
+        controller.add(PacketMessage.fromJson(jsonDecode(msg)));
+        failAfter = DateTime.now().add(const Duration(seconds: 1));
+      }
+    }
 
     void setSubscription(StreamSubscription sub) {
       subscription = sub;
+      failAfter = DateTime.now().add(const Duration(seconds: 5));
       sub.onDone(() {
         if (!retry) return;
         controller.add(DisconnectMessage());
-        setSubscription(connector(url).stream.listen((msg) {
-          if (msg is String) {
-            print(msg);
-            controller.add(PacketMessage.fromJson(jsonDecode(msg)));
-          }
-        }));
+        setSubscription(connector(url).stream.listen(handleMessage));
       });
     }
-
-    setSubscription(connector(url).stream.listen((msg) {
-      if (msg is String) {
-        print(msg);
-        controller.add(PacketMessage.fromJson(jsonDecode(msg)));
-      }
-    }));
+    
+    setSubscription(connector(url).stream.listen(handleMessage));
     return controller.stream;
   }
 }
